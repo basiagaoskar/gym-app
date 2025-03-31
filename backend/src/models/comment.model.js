198d28da159d9fb80f2aca9d707e7c611acdba2d
Oskar Basiaga <basiagaoskar@gmail.com>
2025-03-31 16:19:59 +0200

    added comment, follow, like models and diagram ERD

---
+++ backend/src/models/comment.model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose"
+
+const commentSchema = new mongoose.Schema(
+    {
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+        workout: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Workout",
+            required: true,
+        },
+        content: {
+            type: String,
+            required: true,
+            minlength: 1,
+        },
+    },
+    { timestamps: true }
+);
+
+const Comment = mongoose.model("Comment", commentSchema)
+
+export default Comment
